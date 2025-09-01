07f0c3f0291aad6d3591a2e2f35706c0b99afa2f
thefenixbarbershop30-cmyk <thefenixbarbershop30@gmail.com>
2025-09-01 03:12:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig({
+export default defineConfig({base: process.env.NODE_ENV === "production" ? "/fenix-barber-shop/" : "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
